719132ab0ea0f0808a91d04841247b867196bc29
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-03-31 13:31:55 -0500

    Give 'Name' the ability to track 'activeTheme'.

--- src/app/components/Resume/Name.js
+++ src/app/components/Resume/Name.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import ThemeContext from '../../themes/Themes' 
 
 const Name = () => {
+  const activeTheme = useContext(ThemeContext);
   return (
-    <section className="o-resume__name">
-      <div className="c-name">
-        <h1 className="c-name__first">Jorge</h1>
-        <h1 className="c-name__last">Reyes</h1>
-        <h1 className="c-name__suffix">Jr</h1>
+    <section className={`${activeTheme.className} o-resume__name`}>
+      <div className="c-bio">
+        <h1 className="c-bio__name">Jorge Reyes Jr.</h1>
+        <hr className="c-bio__accent"></hr>
+        <div className="c-bio__profession">
+          <p className="c-profession">Software Engineer</p>
+        </div>
       </div>
     </section>
   )
