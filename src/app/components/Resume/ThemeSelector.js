341167b9fc1f9d85dfce2cf38d75995097187885
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-03-31 13:19:25 -0500

    Create 'o-theme-selector' object (contains radio inputs to set theme for 'Resume').

--- src/app/components/Resume/ThemeSelector.js
+++ src/app/components/Resume/ThemeSelector.js
@@ -3,8 +3,7 @@ import ThemeContext from '../../themes/Themes';
 
 const ThemeSelector = (props) => {
   const allThemes = useContext(ThemeContext);
-  const defaultTheme = applyDefaultTheme();
-  const [activeTheme, setActiveTheme] = useState(defaultTheme);
+  const [activeTheme, setActiveTheme] = useState(applyDefaultTheme());
 
   function applyDefaultTheme() {
     for(let i = 0; i < Object.keys(allThemes).length; i++) {
@@ -28,38 +27,35 @@ const ThemeSelector = (props) => {
     } 
   }
 
+  function generateNewKey() {
+    const limit = 100;
+    const newKey = Math.floor(Math.random() * Math.floor(limit));
+
+    return newKey;
+  }
+
   function handleThemeChange(e) {
     const newTheme = applyNewTheme(e.target.value);
+    
     setActiveTheme(newTheme)
   }
-  
+
   return (
-    <ThemeContext.Provider value={activeTheme}>
-        <form className="o-theme-selection">
-          <div className="radio">
-            <label>
-              <input type="radio" value="Default" checked={activeTheme.themeName === 'Default'} onChange={handleThemeChange} />
-              Default
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-              <input type="radio" value="Jurassic Park" checked={activeTheme.themeName === 'Jurassic Park'} onChange={handleThemeChange}/>
-              Jurassic Park
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-              <input type="radio" value="Operator" checked={activeTheme.themeName === 'Operator'} onChange={handleThemeChange}/>
-              Operator
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-              <input type="radio" value="Tron" checked={activeTheme.themeName === 'Tron'} onChange={handleThemeChange}/>
-              Tron
-            </label>
-          </div>
+    <ThemeContext.Provider value={activeTheme}
+    >
+        <form className="o-theme-selector">
+          {
+            Object.entries(allThemes).map((theme) => {
+              return (
+                <div className="o-theme-selector__radio" key={`${theme[0]}-${generateNewKey()}`}>
+                  <label className="o-theme-selector__label">
+                    <input className="o-theme-selector__input" type="radio" value={theme[0]} checked={activeTheme.themeName === `${theme[0]}`} onChange={handleThemeChange} />
+                    {theme[0]}
+                  </label>
+                </div>
+              )
+            })
+          }
         </form>
         { props.children }
     </ThemeContext.Provider>
