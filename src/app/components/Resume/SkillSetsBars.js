660afe87b644fd3208bcfcfa2170a056327c0f28
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-03-31 13:39:28 -0500

    Create new 'SkillsetsBars' component (to display skills for 'small' screen sizes.

---
+++ src/app/components/Resume/SkillSetsBars.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Skill from './Skill';
+
+const SkillsetsBars = () => {
+  return (
+    <section className="o-resume__skillset o-resume__skillset--bars">
+      <div className="c-data-panel">
+        <h2 className="c-data-panel__title">Skillsets</h2>
+        <div className="c-data-panel__content">
+          <div className="c-snapshot-panel">
+            <div className="c-snapshot-panel__content">
+              {/* SKILLSET CARDS */}
+              <div className="c-skillset__card">
+                <h3 className="c-skillset__title">c-skillset__card</h3>
+                <div className="c-skillset__content">
+                  
+                  <Skill title={`JS`}/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default SkillsetsBars;
