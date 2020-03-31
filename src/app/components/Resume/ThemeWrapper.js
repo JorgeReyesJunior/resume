bf1bcf378c1e5c08180800dd2d66de9f8db384da
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-03-31 13:02:40 -0500

    Create '<ThemeWrapper />' component (contains '<Resume />').

---
+++ src/app/components/Resume/ThemeWrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Resume from './Resume'
+import ThemeSelector from './ThemeSelector';
+
+const ThemeSelectorWrapper = () => {
+  return (
+    <ThemeSelector >
+      <Resume />
+    </ThemeSelector>
+  )
+};
+
+export default ThemeSelectorWrapper;
