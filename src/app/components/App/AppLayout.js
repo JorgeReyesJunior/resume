7183b453787394b1c4804422faac61aa94f310ea
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-02 10:18:19 -0500

    Give the 'AppLayout' access to 'themes'.

--- src/app/components/App/AppLayout.js
+++ src/app/components/App/AppLayout.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import MainScreen from '../Layout/MainScreen';
+import ThemeContext from '../Themes/Themes';
 
 const AppLayout = () => {
+  const activeTheme = useContext(ThemeContext);
+  
   return (
-    <div className="o-app-layout">
+    <div className={`${activeTheme.className} o-app-layout`}>
       <div className="o-app-layout__mainscreen">
         <MainScreen />
       </div>
