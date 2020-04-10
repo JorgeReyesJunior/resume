a53e2570017cb677610041d8e2648a081678e461
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-10 13:25:50 -0500

    Rename component.

---
+++ src/app/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Resume from '../Resume/Resume';
+
+const Layout = () => {  
+  return (
+    <div className={`o-layout`}>
+      <Resume />
+    </div>
+  )
+};
+
+export default Layout;
