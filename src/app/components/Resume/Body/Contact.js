7d4b303f9a0df0aeb85a7d4d85984aa25fca3d9c
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-10 13:33:13 -0500

    Remove unnecessary markup and render new prop data.

--- src/app/components/Resume/Body/Contact.js
+++ src/app/components/Resume/Body/Contact.js
@@ -1,24 +1,18 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
+
   return (
     <section className="o-resume__contact">
-      <div className="c-contact">
-        <div className="c-data-panel">
-          <h2 className="c-data-panel__title">Contact</h2>
-          <div className="c-data-panel__content">
-            <div className="c-snapshot-panel">
-              <div className="c-snapshot-panel__content">
-                <ul className="c-contact__list">
-                  {/* open a new tab with google maps? */}
-                  <li className="c-contact__listing">Area of Operations: <span className="c-contact__listing-anchor">Austin, TX</span></li>
-                  <li className="c-contact__listing">Github: <span className="c-contact__listing-anchor">https://github.com/JorgeReyesJunior</span></li>
-                  <li className="c-contact__listing">Github.io: <span className="c-link c-link--githubio" href="https://jorgereyesjunior.github.io/">jorgereyesjunior.github.io</span></li>
-                  <li className="c-contact__listing">LinkedIn: <span className="c-contact__listing-anchor">linkedin.com/in/jareyesjr</span></li>
-                </ul>
-              </div>
-            </div>
-          </div>
+      <div className={'c-contact'}>
+        <div className="c-snapshot-panel">
+          <h2 className="c-snapshot-panel__title">{props.contact.title}</h2>
+          <p className="c-snapshot-panel__content">
+            <h5>Email:</h5>{props.contact.email}
+            <br></br>
+            <br></br>
+            <h5>LinkedIn:</h5>{props.contact.linkedin}
+          </p>
         </div>
       </div>
     </section>
