c168c6d21951f622c6ed336d286e2206be47e6ca
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-10 16:17:02 -0500

    Add 'github.io' link.

--- src/app/components/Resume/Body/Contact.js
+++ src/app/components/Resume/Body/Contact.js
@@ -6,13 +6,16 @@ const Contact = (props) => {
     <section className="o-resume__contact">
       <div className={'c-contact'}>
         <div className="c-snapshot-panel">
-          <h2 className="c-snapshot-panel__title">{props.contact.title}</h2>
-          <p className="c-snapshot-panel__content">
+          <h3 className="c-snapshot-panel__title">{props.contact.title}</h3>
+          <div className="c-snapshot-panel__content">
             <h5>Email:</h5>{props.contact.email}
             <br></br>
             <br></br>
             <h5>LinkedIn:</h5>{props.contact.linkedin}
-          </p>
+            <br></br>
+            <br></br>
+            <h5>Github:</h5>{props.contact.github}
+          </div>
         </div>
       </div>
     </section>
