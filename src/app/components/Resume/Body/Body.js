4bf9b97956f57f88eeb50d0b36a497c58658b564
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-10 13:26:34 -0500

    Reorganize the resume props.

--- src/app/components/Resume/Body/Body.js
+++ src/app/components/Resume/Body/Body.js
@@ -6,16 +6,14 @@ import Profile from './Profile';
 import Skillsets from './Skillsets';
 
 const Body = (props) => {
-  const resume = props.data.resume;
-
   return (
-    <div className="o-resume__body">
-      <Profile profile={resume.profile} />
-      <Contact contact={resume.contact} />
-      <Education education={resume.education} />
-      <Skillsets skills={resume.skills} />
-      <Experience experience={resume.experience} />
-    </div>
+    <main className="o-resume__body">
+      <Profile profile={props.resume.profile} />
+      <Contact contact={props.resume.contact} />
+      <Education education={props.resume.education} />
+      <Skillsets skills={props.resume.skills} />
+      <Experience experience={props.resume.experience} />
+    </main>
   )
 };
 
