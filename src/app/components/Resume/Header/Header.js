cda4cf44c0948b6d979a29c27e47dd18d0703cae
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-10 13:45:07 -0500

    Write conditions to selectively render different 'o-resume-header' component styles.

--- src/app/components/Resume/Header/Header.js
+++ src/app/components/Resume/Header/Header.js
@@ -1,34 +1,42 @@
 import React from 'react';
-import Name from './Name';
+import Bio from './Bio';
 import Headline from './Headline';
 import Headshot from './Headshot';
 
 const Header = (props) => {
   if (props.omitHeadshotAndHeadline) {
     return (
-      <header className="o-resume__header">
-        <Name />
+      <header className={`o-resume__header`}>
+        <div className={`c-resume__header`}>
+          <Bio bio={props.resume.bio}/>
+        </div>
       </header>
     )
   } else if (props.includeHeadshotOnly) {
     return (
-      <header className="o-resume__header--with-headshot">
-        <Headshot />
-        <Name />
+      <header className={`o-resume__header--with-headshot`}>
+        <div className={`c-resume__header--with-headshot`}>
+          <Headshot />
+          <Bio bio={props.resume.bio} />
+        </div>
       </header>
     )
   } else if (props.includeHeadshotAndHeadline) {
     return (
-      <header className="o-resume__header--with-headshot-and-headline">
+      <header className={`o-resume__header--with-headshot-and-headline`}>
+        <div className={`c-resume__header--with-headshot-and-headline`}>
         <Headshot />
-        <Name />
+        <Bio bio={props.resume.bio} />
         <Headline />
+      </div>
       </header>
     )
   } else {
     return (
-      <header className="o-resume__header">
-        <Name />
+      <header className={`o-resume__header`}>
+        <div className={`c-resume__header`}>
+          <Bio bio={props.resume.bio} />
+        </div>
       </header>
     )
   }
