f827689befb0ba97991756889527ad6310870035
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-10 13:24:49 -0500

    Update comments/components for PROD/DEV env's.

--- src/app/components/App/AppBackground.js
+++ src/app/components/App/AppBackground.js
@@ -1,21 +1,25 @@
-import React, {useContext} from 'react';
-// import Image from '../../../assets/media/images/white-triangles-background.png';
-// import Video from '../../../assets/media/videos/white-triangles-background.mp4';
+import React, { useContext } from 'react';
 import ThemeContext from '../Themes/Themes';
+
+// NOTE: For development only
+import Image from '../../../assets/media/images/white-triangles-background.png';
+import Video from '../../../assets/media/videos/white-triangles-background.mp4';
 
 const AppBackground = () => {
   const activeTheme = useContext(ThemeContext);
 
   return (
     <div className={`${activeTheme.className} o-app-background`}>
-      <div className="o-app-background__overlay"></div>
-      {/* // NOTE: for 'master': 'https://jorgereyesjunior.github.io/src/assets/media/images/white-triangles-background.png' */}
-      {/* // NOTE: for 'development': <img className="o-app-background__image" src={Image} alt="Full screen background image of abstract white triangles"></img> */}
-      <img className="o-app-background__image" src={'https://jorgereyesjunior.github.io/src/assets/media/images/white-triangles-background.png'} alt="Full screen abstract white triangles background"></img>
-      <video className="o-app-background__video" preload="preload" autoPlay="autoplay" loop="loop" muted >
-        {/* // NOTE: for 'master': https://jorgereyesjunior.github.io/src/assets/media/videos/white-triangles-background.mp4 */}
-        {/* // NOTE: for 'development': <source src={Video} type="video/mp4"></source> */}
-        <source src={'https://jorgereyesjunior.github.io/src/assets/media/videos/white-triangles-background.mp4'} type="video/mp4"></source>
+      <div className={`o-app-background__overlay`}></div>
+      {/* NOTE: for 'production' only: */}
+      {/* <img className="o-app-background__image" src={'https://jorgereyesjunior.github.io/src/assets/media/images/white-triangles-background.png'} alt="Full screen abstract white triangles background"></img> */}
+      {/* NOTE: for 'development' only: */}
+      <img className={`o-app-background__image`} src={Image} alt="Full screen abstract white triangles background"></img>
+      <video className={`o-app-background__video`} preload="preload" autoPlay="autoplay" loop="loop" muted >
+        {/* NOTE: for 'production' only: */}
+        {/* <source src={'https://jorgereyesjunior.github.io/src/assets/media/videos/white-triangles-background.mp4'} type="video/mp4"></source> */}
+        {/* NOTE: for 'development' only: */}
+        <source src={Video} type="video/mp4"></source>
       </video>
     </div>
   )
