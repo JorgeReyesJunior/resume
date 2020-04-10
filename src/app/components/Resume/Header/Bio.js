c8bd9411375f134be327236a824a55d15fb25c58
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-10 13:27:03 -0500

    Create the resume's 'bio' component.

---
+++ src/app/components/Resume/Header/Bio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Bio = (props) => {
+  console.log(props.bio)
+  const fullName = `${props.bio.first} ${props.bio.last} ${props.bio.suffix}`;
+
+  return (
+    <section className={`o-resume__bio`}>
+      <div className={`c-resume-bio`}>
+        <h1 className={`c-resume-bio__name`}>{fullName}</h1>
+        <hr className={`c-resume-bio__accent`}></hr>
+        <h4 className={`c-resume-bio__profession`}>{props.bio.profession}</h4>
+      </div>
+    </section>
+  )
+};
+
+export default Bio;
