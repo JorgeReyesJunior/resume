4532676770f9e8d7b59812b269ba56666cac877f
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-11 19:58:58 -0500

    Add 'redacted' resume props.

--- src/app/components/Resume/Body/Body.js
+++ src/app/components/Resume/Body/Body.js
@@ -8,11 +8,11 @@ import Skillsets from './Skillsets';
 const Body = (props) => {
   return (
     <main className="o-resume__body">
-      <Profile profile={props.resume.profile} />
-      <Contact contact={props.resume.contact} />
-      <Education education={props.resume.education} />
-      <Skillsets skills={props.resume.skills} />
-      <Experience experience={props.resume.experience} />
+      <Profile profile={props.resume.profile} profile--redacted={props.resume['profile--redacted']} />
+      <Contact contact={props.resume.contact}contact--redacted={props.resume['contact--redacted']} />
+      <Education education={props.resume.education} education--redacted={props.resume['education--redacted']} />
+      <Skillsets skills={props.resume.skills} skills--redacted={props.resume['skills--redacted']} />
+      <Experience experience={props.resume.experience} experience--redacted={props.resume['experience--redacted']} />
     </main>
   )
 };
