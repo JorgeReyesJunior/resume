782e0d14aaa57d6d5ce654a7b8b64d92f2ede9cb
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-11 20:04:58 -0500

    Add support for 'operator' theme.

--- src/app/components/Resume/Header/Bio.js
+++ src/app/components/Resume/Header/Bio.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ThemeContext from '../../Themes/Themes';
+import utilities from '../../../utilities/utilities';
 
 const Bio = (props) => {
-  console.log(props.bio)
+  const activeTheme = useContext(ThemeContext);
+
   const fullName = `${props.bio.first} ${props.bio.last} ${props.bio.suffix}`;
 
-  return (
-    <section className={`o-resume__bio`}>
-      <div className={`c-resume-bio`}>
-        <h1 className={`c-resume-bio__name`}>{fullName}</h1>
-        <hr className={`c-resume-bio__accent`}></hr>
-        <h5 className={`c-resume-bio__profession`}>{props.bio.profession}</h5>
-      </div>
-    </section>
-  )
+  const redactedFullName = `${props['bio--redacted'].first} ${props['bio--redacted'].last} ${props['bio--redacted'].suffix}`;
+
+  if (activeTheme.themeName === 'Operator') {
+    return (
+      <section className={`o-resume__bio`}>
+        <div className={`c-resume-bio`}>
+          <h1 className={`c-resume-bio__name`}>{utilities.renderRedactedContent(redactedFullName)}</h1>
+          <hr className={`c-resume-bio__accent`}></hr>
+          <h5 className={`c-resume-bio__profession`}>{utilities.renderRedactedContent(props['bio--redacted'].profession)}</h5>
+        </div>
+      </section>
+    )
+  } else {
+    return (
+      <section className={`o-resume__bio`}>
+        <div className={`c-resume-bio`}>
+          <h1 className={`c-resume-bio__name`}>{fullName}</h1>
+          <hr className={`c-resume-bio__accent`}></hr>
+          <h5 className={`c-resume-bio__profession`}>{props.bio.profession}</h5>
+        </div>
+      </section>
+    )
+  }
 };
 
 export default Bio;
