3dc090b49a5167c8b5e17d14e1c21332ef731d30
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-11 20:02:48 -0500

    Create/Add 'renderRedactedContent' utility.

--- src/app/utilities/utilities.js
+++ src/app/utilities/utilities.js
@@ -1,3 +1,5 @@
+import React, { Fragment } from 'react';
+
 function generateNewKey(val) {
   let newKey; 
 
@@ -10,6 +12,38 @@ function generateNewKey(val) {
   return newKey;
 }
 
+/**
+ * 
+ * Render redacted content.
+ * @constructor
+ * @param {string} redactedContent - The content to be rendered (contains [REDACTIONS]).
+ * 
+ */
+function renderRedactedContent(redactedContent) {
+  // Identify a `[REDACTION]`.
+  const redaction = new RegExp(/(\[REDACTED])/, 'g')
+
+  // Separate all identified `[REDACTION]`'s from the `redactedContent`. 
+  const redactions = redactedContent.split(redaction);
+
+  function redactWord(word, i) {
+    return word.match(redaction) ? <b className={`o-redaction`} key={`redact-${generateNewKey(i)}`}>{word}</b> : word
+  }
+
+  return (
+    <Fragment>
+      {
+        redactions.map(
+          (redaction, i) => (
+            redactWord(redaction, i)
+          )
+        )
+      }
+    </Fragment>
+  );
+}
+
 export default {
-  generateNewKey
+  generateNewKey,
+  renderRedactedContent
 };
