187b2dc10e2ae6d171be22c6fd9fa759def4f617
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-04-11 20:05:27 -0500

    Add support for 'operator' theme.

--- src/app/components/Resume/Body/Skillsets.js
+++ src/app/components/Resume/Body/Skillsets.js
@@ -1,9 +1,12 @@
-import React, {Fragment, useEffect, useState} from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
 // import SkillsetsBars from './SkillSetsBars';
 // import SkillsetsCards from './SkillSetsCards';
+import ThemeContext from '../../Themes/Themes';
 import utilities from '../../../utilities/utilities';
 
 const Skillsets = (props) => {
+  const activeTheme = useContext(ThemeContext);
+
   // const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
 
   // const updateMedia = () => {
@@ -19,65 +22,129 @@ const Skillsets = (props) => {
   // const { primary } = props.skills;
   // console.log(primary)
 
-  return (
-    <Fragment>
+  // TODO
+  //       {
+  //         isDesktop ? (
+  //           <SkillsetsCards primarySkillset={props.skills.primary} secondarySkillset={props.skills.secondary} />
+  //         ) : (
+  //           <SkillsetsBars primarySkillset={props.skills.primary} secondarySkillset={props.skills.secondary} />
+  //         )}
 
-      {/* TODO */}
-      {/* {
-        isDesktop ? (
-          <SkillsetsCards primarySkillset={props.skills.primary} secondarySkillset={props.skills.secondary} />
-        ) : (
-          <SkillsetsBars primarySkillset={props.skills.primary} secondarySkillset={props.skills.secondary} />
-        )} */}
-        <section className="o-resume__skillset">
-          <div className="c-skillset">
-            <div className="c-snapshot-panel">
-              <h3 className="c-snapshot-panel__title">{props.skills.title}</h3>
-              <div className="c-snapshot-panel__content">
-                <h5 className={'c-skillset__title--primary'}>Primary Skillset:</h5>
-                <ul className='c-list c-list--primary-skillset'>
-                  {
-                    Object.entries(props.skills.primary).map((primarySkill, i) => {
-                      const skillType = primarySkill[0];
-
-                      // Insert a comma after each listed skill.
-                      const skillList = primarySkill[1].reduce((acc, val) => {
-                        acc.push(val);
-                        acc.push(", ");
-
-                        return acc;
-                      }, []);
-
-                      // Replace trailing comma with a period.
-                      skillList.pop()
-                      skillList.push(".");
-
-                      return (
-                        <li key={utilities.generateNewKey(i)} className='s-temp-list__listing'>
-                          <p>{skillType}: {skillList}</p>
-                        </li>
-                      )
-                    })
-                  }
-                </ul>
-                <br></br>
-                <h5 className={'c-skillset__title--secondary'}>Secondary Skillsets:</h5>
-                  <ul className='c-list c-list--secondary-skillset'>
+  if (activeTheme.themeName === 'Operator') {
+    return (
+      <Fragment>
+          <section className="o-resume__skillset">
+            <div className="c-skillset">
+              <div className="c-snapshot-panel">
+                <h3 className="c-snapshot-panel__title">{utilities.renderRedactedContent(props['skills--redacted'].title)}</h3>
+                <div className="c-snapshot-panel__content">
+                  <h5 className={'c-skillset__title--primary'}>Primary Skillset:</h5>
+                  <ul className='c-list c-list--primary-skillset'>
                     {
-                      Object.entries(props.skills.secondary).map((secondarySkill, i) => {
+                      Object.entries(props['skills--redacted'].primary).map((primarySkill, i) => {
+                        const skillType = primarySkill[0];
+  
+                        // Insert a comma after each listed skill.
+                        const skillList = primarySkill[1].reduce((acc, val) => {
+                          acc.push(val);
+                          acc.push(", ");
+  
+                          return acc;
+                        }, []);
+  
+                        // Replace trailing comma with a period.
+                        skillList.pop()
+                        skillList.push(".");
+                        
+                        // Convert Array values into string (to detect redactions).
+                        const redactedSkillList = skillList.join('');
+
+                        const redactedContent = utilities.renderRedactedContent(redactedSkillList);
+  
                         return (
                           <li key={utilities.generateNewKey(i)} className='s-temp-list__listing'>
-                            <p>- {secondarySkill[1]}</p>
+                            <p>{skillType}: {redactedContent}</p>
                           </li>
                         )
                       })
                     }
                   </ul>
+                  <br></br>
+                  <h5 className={'c-skillset__title--secondary'}>Secondary Skillsets:</h5>
+                    <ul className='c-list c-list--secondary-skillset'>
+                      {
+                        Object.entries(props['skills--redacted'].secondary).map((secondarySkill, i) => {
+
+                          const redactedContent = utilities.renderRedactedContent(secondarySkill[1]);
+                          
+                          return (
+                            <li key={utilities.generateNewKey(i)} className='s-temp-list__listing'>
+                              <p>- {redactedContent}</p>
+                            </li>
+                          )
+                        })
+                      }
+                    </ul>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-    </Fragment>
-  );
+          </section>
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+          <section className="o-resume__skillset">
+            <div className="c-skillset">
+              <div className="c-snapshot-panel">
+                <h3 className="c-snapshot-panel__title">{props.skills.title}</h3>
+                <div className="c-snapshot-panel__content">
+                  <h5 className={'c-skillset__title--primary'}>Primary Skillset:</h5>
+                  <ul className='c-list c-list--primary-skillset'>
+                    {
+                      Object.entries(props.skills.primary).map((primarySkill, i) => {
+                        const skillType = primarySkill[0];
+  
+                        // Insert a comma after each listed skill.
+                        const skillList = primarySkill[1].reduce((acc, val) => {
+                          acc.push(val);
+                          acc.push(", ");
+  
+                          return acc;
+                        }, []);
+  
+                        // Replace trailing comma with a period.
+                        skillList.pop()
+                        skillList.push(".");
+  
+                        return (
+                          <li key={utilities.generateNewKey(i)} className='s-temp-list__listing'>
+                            <p>{skillType}: {skillList}</p>
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+                  <br></br>
+                  <h5 className={'c-skillset__title--secondary'}>Secondary Skillsets:</h5>
+                    <ul className='c-list c-list--secondary-skillset'>
+                      {
+                        Object.entries(props.skills.secondary).map((secondarySkill, i) => {
+                          return (
+                            <li key={utilities.generateNewKey(i)} className='s-temp-list__listing'>
+                              <p>- {secondarySkill[1]}</p>
+                            </li>
+                          )
+                        })
+                      }
+                    </ul>
+                </div>
+              </div>
+            </div>
+          </section>
+      </Fragment>
+    );
+  }
+  
 }
 export default Skillsets;
