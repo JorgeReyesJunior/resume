71ac9a3a205d1a0e30acecb66e720d7e9cea00b5
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-05-05 01:04:14 -0700

    HOTFIX: Update backgruond media (img).

--- src/app/components/App/AppBackground.js
+++ src/app/components/App/AppBackground.js
@@ -7,7 +7,7 @@ const AppBackground = () => {
   return (
     <div className={`${activeTheme.className} o-app-background`}>
       <div className={`o-app-background__overlay`}></div>
-      <img className="o-app-background__image" src={'https://jorgereyesjunior.github.io/src/assets/media/images/white-triangles-background.png'} alt="Full screen abstract white triangles background"></img>
+      <img className="o-app-background__image" src={'https://jorgereyesjunior.github.io/src/assets/media/images/white-triangles-background.webp'} alt="Full screen abstract white triangles background"></img>
       <video className={`o-app-background__video`} preload="preload" autoPlay="autoplay" loop="loop" muted >
         <source src={'https://jorgereyesjunior.github.io/src/assets/media/videos/white-triangles-background.mp4'} type="video/mp4"></source>
       </video>
