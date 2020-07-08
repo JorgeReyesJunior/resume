be0c50455bda10ece662a22dc6ff607ec39f89d0
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-07-08 11:19:15 -0700

    Fix background image.

--- src/app/components/App/AppBackground.js
+++ src/app/components/App/AppBackground.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import ThemeContext from '../Themes/Themes';
-
 // NOTE: For development only
 // import Image from '../../../assets/media/images/white-triangles-background.png';
 // import Video from '../../../assets/media/videos/white-triangles-background.mp4';
@@ -11,15 +10,9 @@ const AppBackground = () => {
   return (
     <div className={`${activeTheme.className} o-app-background`}>
       <div className={`o-app-background__overlay`}></div>
-      {/* NOTE: for 'production' only: */}
-      <img className="o-app-background__image" src={'https://jorgereyesjunior.github.io/src/assets/media/images/white-triangles-background.png'} alt="Full screen abstract white triangles background"></img>
-      {/* NOTE: for 'development' only: */}
-      {/* <img className={`o-app-background__image`} src={Image} alt="Full screen abstract white triangles background"></img> */}
+      <img className="o-app-background__image" src={'https://jorgereyesjunior.github.io/src/assets/media/images/white-triangles-background.webp'} alt="Full screen abstract white triangles background"></img>
       <video className={`o-app-background__video`} preload="preload" autoPlay="autoplay" loop="loop" muted >
-        {/* NOTE: for 'production' only: */}
         <source src={'https://jorgereyesjunior.github.io/src/assets/media/videos/white-triangles-background.mp4'} type="video/mp4"></source>
-        {/* NOTE: for 'development' only: */}
-        {/* <source src={Video} type="video/mp4"></source> */}
       </video>
     </div>
   )
