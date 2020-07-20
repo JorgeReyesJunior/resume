c425ea0c0e27918cf8fc176a5823d514964565ef
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-07-19 20:19:39 -0700

    Update github username.

--- src/app/components/App/AppBackground.js
+++ src/app/components/App/AppBackground.js
@@ -1,19 +1,15 @@
 import React, { useContext } from 'react';
 import ThemeContext from '../Themes/Themes';
 
-// NOTE: For development only
-// import Image from '../../../assets/media/images/white-triangles-background.png';
-// import Video from '../../../assets/media/videos/white-triangles-background.mp4';
-
 const AppBackground = () => {
   const activeTheme = useContext(ThemeContext);
 
   return (
     <div className={`${activeTheme.className} o-app-background`}>
       <div className={`o-app-background__overlay`}></div>
-      <img className="o-app-background__image" src={'https://jorgereyesjunior.github.io/src/assets/media/images/white-triangles-background.webp'} alt="Full screen abstract white triangles background"></img>
+      <img className="o-app-background__image" src={'https://jorgeareyesjr.github.io/src/assets/media/images/white-triangles-background.webp'} alt="Full screen abstract white triangles background"></img>
       <video className={`o-app-background__video`} preload="preload" autoPlay="autoplay" loop="loop" muted >
-        <source src={'https://jorgereyesjunior.github.io/src/assets/media/videos/white-triangles-background.mp4'} type="video/mp4"></source>
+        <source src={'https://jorgeareyesjr.github.io/src/assets/media/videos/white-triangles-background.mp4'} type="video/mp4"></source>
       </video>
     </div>
   )
